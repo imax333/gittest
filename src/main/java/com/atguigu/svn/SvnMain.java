bb96560aa6d99d4564e9a38757e8ab858e10677a
tom <tom@qq.com>
2019-04-12 20:50:31 +0800

    测试冲突

--- src/main/java/com/atguigu/svn/SvnMain.java
+++ src/main/java/com/atguigu/svn/SvnMain.java
@@ -4,7 +4,7 @@ public class SvnMain {
 
 	public static void main(String[] args) {
 		System.out.println("ccccc");// BBB
-		System.out.println("ccccc");// AAA
+		System.out.println("ccccc");// AAA ccc
 		
 	}
 }
