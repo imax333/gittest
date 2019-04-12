4de066602f33fc3b4b1b07008d9feb8b7aad5a39
tom <tom@qq.com>
2019-04-12 21:07:41 +0800

    hotfix2

--- src/main/java/com/atguigu/svn/SvnMain.java
+++ src/main/java/com/atguigu/svn/SvnMain.java
@@ -5,7 +5,7 @@ public class SvnMain {
 	public static void main(String[] args) {
 		System.out.println("ccccc");// BBB
 		System.out.println("ccccc");// AAA ccc
-		System.out.println("ccccc");// AAA xxx edit by hotfix 
+		System.out.println("ccccc");// AAA xxx edit by hotfix222222 
 		
 	}
 }
