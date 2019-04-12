bb7da9751cf83941ebe135289e29bc33c8459f7b
tom <tom@qq.com>
2019-04-12 20:21:42 +0800

    新建的工程

---
+++ src/main/java/com/atguigu/svn/SvnMain.java
@@ -0,0 +1,10 @@
+package com.atguigu.svn;
+
+public class SvnMain {
+
+	public static void main(String[] args) {
+		System.out.println("ccccc");// BBB
+		System.out.println("ccccc");// AAA
+		
+	}
+}
